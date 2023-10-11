6db29e25d911ba81259fed391a07bfaca08f6c59
lucasmapaiva <86642899+LucasMaPaiva@users.noreply.github.com>
2023-10-10 22:15:49 -0400

    enabling h2 database

--- src/main/java/br/com/lucaspaiva/todolist/user/UserModel.java
+++ src/main/java/br/com/lucaspaiva/todolist/user/UserModel.java
@@ -1,7 +1,12 @@
 package br.com.lucaspaiva.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
-    public String username;
-    public String name;
-    public String password;
+
+    private String username;
+    private String name;
+    private String password;
+
 }
